f265261e1bd4a8d5a5139a7c4f26c44315f76c2c
asdword <asdword1200@outlook.com>
2022-02-24 15:42:46 +0330

    clean cs files

--- GeoJson.cs
+++ GeoJson.cs
@@ -9,6 +9,3 @@ public class GeoProperties{
     public string name { get; set; }
     public string province { get; set; }
 }
-
-
-// type":"Feature","properties":{"@id":"relation/269911","admin_level":"5","name":"شهرستان شیروان","place":"county","type":"boundary"},"geometry"
